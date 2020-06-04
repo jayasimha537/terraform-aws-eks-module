343b0627d6e8f1a03a180a8a2fb548184f81fbd5
jayasimha537 <simha.ggd@gmail.com>
2020-06-04 21:07:54 +0530

    updated eks cluster name

--- example/main.tf
+++ example/main.tf
@@ -4,14 +4,16 @@ provider "aws" {
 }
 
 locals {
+  #eks_cluster_name = "mycluster"
   projectName = "myproject"
+  environmentName = "dev"
 }
 
 
 module "dev_vpc" {
   source = "git::https://github.com/jayasimha537/terraform-aws-vpc-module.git"
   projectName = local.projectName
-  environmentName = "dev"
+  environmentName = local.environmentName
   vpc_cidr = "10.0.0.0/16"
   public_subnets_count = 2
   private_subnets_count = 2
@@ -19,10 +21,10 @@ module "dev_vpc" {
   public_outbound_acl_rules = var.public_outbound_acl_rules
   private_inbound_acl_rules = var.private_inbound_acl_rules
   private_outbound_acl_rules = var.private_outbound_acl_rules
-  enable_nat_gw = false
+  enable_nat_gw = true
   default_tags = {
     "Author" = "Jayasimha"
-    "kubernetes.io/cluster/${local.projectName}" = "shared"
+    "kubernetes.io/cluster/${local.projectName}_${local.environmentName}_eks_cluster" = "shared"
   }
 
   public_subnet_tags = {
@@ -34,7 +36,7 @@ module "dev_vpc" {
 }
 
 module "eks_cluster" {
-  source = "../../"
+  source = "git::https://github.com/jayasimha537/terraform-aws-eks-module.git"
   vpc_id = module.dev_vpc.vpc_id
   vpc_cidr = module.dev_vpc.vpc_cidr
   private_subnet_id_list = module.dev_vpc.private_subnet_id_list
